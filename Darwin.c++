007988a3e026638dad6d03a81431cf4eb314ac0d
nelmiux <npererar@utexas.edu>
2015-10-30 13:50:29 -0500

    Adding Initial Files

---
+++ Darwin.c++
@@ -0,0 +1,14 @@
+// --------
+// includes
+// --------
+
+#include <cassert>   // assert
+#include <cstdlib>   // rand, srand
+#include <iostream>  // cout, endl
+#include <stdexcept> // invalid_argument, out_of_range
+
+#include "Darwin.h"
+
+using namespace std;
+
+
---
+++ Darwin.h
@@ -0,0 +1,17 @@
+#ifndef Darwin_h
+#define Darwin_h
+
+// --------
+// includes
+// --------
+
+#include <cassert>   // assert
+#include <cstdlib>   // rand, srand
+#include <iostream>  // cout, endl
+#include <stdexcept> // invalid_argument, out_of_range
+
+using namespace std;
+
+
+
+#endif // Darwin_h
